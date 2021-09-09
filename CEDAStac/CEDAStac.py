a8865d447328c60e8b2638d52a3f2d280d43a4f5
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-09-09 16:06:54 +0100

    SearchFutures issue#46

--- CEDAStac/CEDAStac.py
+++ CEDAStac/CEDAStac.py
@@ -5,6 +5,8 @@ __contact__ = 'kazi.mahir@stfc.ac.uk'
 __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
 
+import json
+
 import requests
 from stac.stac import STAC
 import warnings
@@ -18,7 +20,14 @@ class CEDAStacClient:
     def __init__(self):
         self._url = "https://stac-elasticsearch-master.130.246.131.9.nip.io"
         self.Client = STAC(url=self._url, verify=False)
+        self._headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
         warnings.simplefilter('ignore')
+
+    def catalog(self):
+        return self.Client.catalog
 
     def get_collections(self):
         collections = self.Client.collections
@@ -38,15 +47,53 @@ class CEDAStacClient:
         itemcollection = collection.get_items(filter=query)
         return itemcollection
 
-    def search(self, **query):
-        return self.Client.search(query)
+    def search(self, freetext: str = None, doctype: str = 'item', **query):
+        if freetext:
+            return self.search(q=freetext, doctype=doctype, **query)
+
+        if 'properties' in query.keys():
+            filter = query.pop('properties', None)
+            return self.post_search(query=query, filter=filter, doctype=doctype)
+
+        if {'bbox', 'intersects'}.intersection(query.keys()):
+            return self.post_search(query=query, doctype=doctype)
+
+        result = self.Client.search(filter=query)
+        if doctype == 'collection':
+            self.loop_results(result)
+        else:
+            return result
+
+    def post_search(self, query: dict, filter: dict = None, doctype: str = 'item'):
+        if filter:
+            query['filter'] = filter
+        data = json.dumps(query)
+        response = requests.post(url=f"{self._url}/search", data=data, verify=False, headers=self._headers)
+        result = response.json()
+        if doctype == 'collection':
+            self.loop_results(result)
+        else:
+            return result
+
+    def loop_results(self, page: dict, collections: set = set([])):
+        collections.update([item.get('collection') for item in page.get('features')])
+        if len(collections) == len(self.catalog()):
+            return list(collections)
+        if page.get('links'):
+            link = page.get('links')[-1]
+            if link.get('rel') == 'next':
+                response = requests.get(url=link.get('href'), verify=False)
+                response = response.json()
+                return self.loop_results(page=response, collections=collections)
+        print(collections)
+        return collections
 
     def queryables(self):
-        response = requests.get(f"{self._url}/queryables")
+        response = requests.get(f"{self._url}/queryables", verify=False)
         return response.json()
 
     def collection_queryables(self, collection_id: str):
-        response = requests.get(f"{self._url}/collections/{collection_id}/queryables")
+        response = requests.get(f"{self._url}/collections/{collection_id}/queryables", verify=False)
         return response.json()
 
     def conformance_classes(self):
